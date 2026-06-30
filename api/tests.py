daf6c6841e8aced45db860db2a4f09d51bd35db4
Diego Del Castillo <diego12.ddc@gmail.com>
2026-06-29 22:32:09 -0500

    fix: resolve ruff linting errors

--- api/tests.py
+++ api/tests.py
@@ -13,23 +13,28 @@ class TestUserView(APITestCase):
     def test_post(self):
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(json.loads(response.content), {"id": 2, "name":"Test2", "dni":"09876543211"})
+        expected = {"id": 2, "name": "Test2", "dni": "09876543211"}
+        self.assertEqual(json.loads(response.content), expected)
         self.assertEqual(User.objects.count(), 2)
 
     def test_get_list(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(json.loads(response.content)), 1)
+        result = json.loads(response.content)
+        self.assertEqual(len(result), 1)
 
     def test_get(self):
         response = self.client.get(self.url + '1/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content), {"id": 1, "name":"Test1", "dni":"09876543210"})
+        expected = {"id": 1, "name": "Test1", "dni": "09876543210"}
+        self.assertEqual(json.loads(response.content), expected)
 
     def test_post_duplicate_dni(self):
-        response = self.client.post(self.url, {'name': 'TestDuplicate', 'dni': '09876543210'}, format='json')
+        data = {'name': 'TestDuplicate', 'dni': '09876543210'}
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(json.loads(response.content), {'detail': 'User already exists'})
+        expected = {'detail': 'User already exists'}
+        self.assertEqual(json.loads(response.content), expected)
 
     def test_health(self):
         response = self.client.get(reverse('health'))
